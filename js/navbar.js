dd387b88a55fe362afcd8ebb1aa6c338a5c14480
Anton Drozd <anton.drozd.95@gmail.com>
2019-01-06 18:46:15 +0200

    Add navbar behavior

--- js/navbar.js
+++ js/navbar.js
@@ -1,5 +1,8 @@
 export default function activateNavbarBehavior() {
   let scrollPosition = 0;
+  const $sections = $('section');
+  const $nav = $('nav');
+  const navHeight = $nav.outerHeight();
 
   $(window).on('scroll', function() {
     if ($(this).scrollTop() < scrollPosition) {
@@ -9,5 +12,16 @@ export default function activateNavbarBehavior() {
       $('.navbar').fadeOut();
       scrollPosition = $(this).scrollTop();
     }
+
+    $sections.each(function() {
+      const top = $(this).offset().top - navHeight;
+      const bottom = top + $(this).outerHeight();
+      const sectionId = $(this).attr('id');
+
+      if (scrollPosition >= top && scrollPosition <= bottom) {
+        $nav.find('.nav-link').removeClass('active');
+        $nav.find(`a[href="#${sectionId}"]`).addClass('active');
+      }
+    });
   });
 }
